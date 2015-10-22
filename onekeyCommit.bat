e9aa8d53a58835619991cbfa6e0db25587bfb122
jassq <1252348575@qq.com>
2015-10-22 19:59:14 +0800

    auto latest commit

---
+++ onekeyCommit.bat
@@ -0,0 +1,12 @@
+git add app.coffee
+git add gulpfile.coffee
+git add onekeyCommit.bat
+git add package.json
+git add README.md
+git add doc
+git add common
+git add con*
+git add public
+git add views
+git commit -m "auto latest commit"
+git push
